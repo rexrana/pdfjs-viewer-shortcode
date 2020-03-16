a83b32b4d62962b8ffd1345219c72a21f6367e80
Peter Hebert <peter@peterhebert.com>
2020-03-16 18:22:17 -0400

    conditionally load block only if class exists

--- pdfjs-viewer-shortcode.php
+++ pdfjs-viewer-shortcode.php
@@ -18,16 +18,18 @@ define( 'PDFJS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 // verify that local Composer autoloader exists
 if (file_exists(PDFJS_PLUGIN_DIR . 'vendor/autoload.php')) {
-    require_once(PDFJS_PLUGIN_DIR . 'vendor/autoload.php');
+	require_once(PDFJS_PLUGIN_DIR . 'vendor/autoload.php');
 }
-
 
 require_once PDFJS_PLUGIN_DIR . 'inc/settings.php';
 require_once PDFJS_PLUGIN_DIR . 'inc/misc.php';
 require_once PDFJS_PLUGIN_DIR . 'inc/shortcode.php';
 require_once PDFJS_PLUGIN_DIR . 'inc/media-button.php';
-require_once PDFJS_PLUGIN_DIR . 'inc/block.php';
 
+// only load block if class found
+if( class_exists('Carbon_Fields\Container\Container') ) {
+	require_once PDFJS_PLUGIN_DIR . 'inc/block.php';
+}
 // load WPBakery support if it is present.
 if ( class_exists( 'Vc_Manager' ) && defined( 'WPB_VC_VERSION' ) ) {
 	require_once PDFJS_PLUGIN_DIR . 'inc/vc-map.php';
